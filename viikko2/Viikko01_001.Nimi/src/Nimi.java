17355775ac114dc5bf06457472eec806002727eb
Matti Luukkainen <mluukkai@iki.fi>
2016-10-29 16:35:52 +0300

    initial

---
+++ viikko2/Viikko01_001.Nimi/src/Nimi.java
@@ -0,0 +1,11 @@
+public class Nimi {
+    
+    public static void main(String[] args) {
+        // Kirjoita ohjelmasi tähän alle
+
+        // BEGIN SOLUTION
+        System.out.println("Erkki Esimerkki");
+        // END SOLUTION
+    }
+
+}
