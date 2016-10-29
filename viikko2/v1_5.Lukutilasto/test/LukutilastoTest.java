d2f901d77e3965e3454f230c843eafe3cc258ee0
Matti Luukkainen <mluukkai@iki.fi>
2016-10-29 16:57:15 +0300

    v2-5

---
+++ viikko2/v1_5.Lukutilasto/test/LukutilastoTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import fi.helsinki.cs.tmc.edutestutils.Points;
+
+@Points("1-5")
+public class LukutilastoTest {
+
+    @Test
+    public void alussaSummaOnNolla() {
+        Lukutilasto tilasto = new Lukutilasto();
+        assertEquals(0, tilasto.summa());
+    }
+}
